d01c7b3b7ce6e6854928a160eb065f5502fc6e0d
1346818327 <1346818327@qq.com>
2024-12-01 18:52:00 +0800

    12.1修改

--- src/router/index.js
+++ src/router/index.js
@@ -5,31 +5,33 @@ import Read from '@/views/Read.vue'
 
 
 //教师端路由
-import Tea from '@/views/Teacher.vue'
+const Tea = () => import('@/views/Teacher.vue')
 //教师端总览
-import TeacherMain from '@/components/TeacherMain.vue';
+const TeacherMain = () => import('@/components/TeacherMain.vue')
 //教师端课程管理
-import TeacherCoursemanage from '@/components/TeacherCoursemanage.vue';
-import TeacherCourseDetail from '@/components/TeacherCourseDetail.vue';
+const TeacherCoursemanage = () => import('@/components/TeacherCoursemanage.vue')
+const TeacherCourseDetail = () => import('@/components/TeacherCourseDetail.vue')
 //教师端学生管理
-import TeacherClassManage from '@/components/TeacherClassManage.vue';
+const TeacherClassManage = () => import('@/components/TeacherClassManage.vue')
 //教师端日程管理
-import Datamanage from '@/components/Datamanage.vue';
-import TeacherClassDetail from '@/components/TeacherClassDetail.vue';
-//教师端作业评估
-import TeacherHomework from '@/components/TeacherHomework.vue';
-//教师端文件上传
-import TeacherUpload from '@/components/TeacherUpload.vue';
+const Datamanage = () => import('@/components/Datamanage.vue')
+const TeacherClassDetail = () => import('@/components/TeacherClassDetail.vue')
 
+//教师端作业评估
+const TeacherHomework = () => import('@/components/TeacherHomework.vue')
+//教师端文件上传
+const TeacherUpload = () => import('@/components/TeacherUpload.vue')
 //学生端路由
-import Std from '@/views/Std.vue';
+const Std = () => import('@/views/Std.vue')
 //学生端总览
-import StdMain from '@/components/StdMain.vue';
+const StdMain = () => import('@/components/StdMain.vue')
+
 //学生端课程概况
-import StdCoursemanager from '@/components/StdCoursemanager.vue';
-import StdCourseDetail from '@/components/StdCourseDetail.vue';
+const StdCoursemanager = () => import('@/components/StdCoursemanager.vue')
+const StdCourseDetail = () => import('@/components/StdCourseDetail.vue')
+
 //学生端作业
-import StdHomework from '@/components/StdHomework.vue';
+const StdHomework = () => import('@/components/StdHomework.vue')
 
 
 
